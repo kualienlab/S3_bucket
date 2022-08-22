b048554c748dd119a6ed6090353126c8158f0822
AronShrestha <aronshrestha498@gmail.com>
2022-08-22 15:54:06 +0545

    added simple dummy list views and detail views

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from fastapi import FastAPI,File,UploadFile
 import os
 
 app = FastAPI()
-dest='/'
+
 
 @app.post("/insertfile")
 def upload(file:UploadFile,des='/home/aroooon/Documents/fastapi/static'):
+    """
+    statically adding the file path (des) where the provided file will be saved
+    """
     fullpath = os.path.join(des,file.filename)
   
     with open(fullpath,"wb+") as file_c:
@@ -15,5 +18,19 @@ def upload(file:UploadFile,des='/home/aroooon/Documents/fastapi/static'):
     return f"file {file.filename} saved at location {fullpath}"
 
 
+@app.get("/list")
+def list_files():
+    files=[]
+    for file in os.listdir('./static'):
+        files.append(file)
+    return files
 
+
+@app.get("/detail_view/{file_name}")
+def detail_file(file_name:str):
+    file_path=os.path.join(os.getcwd()+'/static')
+    for file in os.listdir(file_path):
+        if file==file_name:
+            return os.path.join(file_path,file)
+    return "Not found"
 
